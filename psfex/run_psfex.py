e6384124261e360cdf5ccf92624bdda06073117a
Mike Jarvis <michael@jarvis.net>
2014-09-01 15:12:22 -0400

    Add code to apply tape bump exclusions

--- psfex/run_psfex.py
+++ psfex/run_psfex.py
@@ -50,6 +50,9 @@ parser.add_argument('--filt_file',
 parser.add_argument('--star_file',
                     default='/astro/u/mjarvis/rmjarvis/DESWL/psfex/sex.nnw',
                     help='name of sextractor star file')
+parser.add_argument('--tapebump_file',
+                    default='/astro/u/mjarvis/rmjarvis/DESWL/psfex/mask_edited.txt',
+                    help='name of tape bump file')
 
 # options
 parser.add_argument('--rm_files',default=1, type=int,
@@ -62,9 +65,41 @@ parser.add_argument('--mag_cut',default=-1,type=float,
                     help='remove the top mags using mag_auto')
 parser.add_argument('--nstars',default=10,type=int,
                     help='use median of brightest nstars for min mag')
+parser.add_argument('--use_tapebumps',default=1,type=int,
+                    help='avoid stars in or near tape bumps')
+parser.add_argument('--tapebump_extra',default=2,type=float,
+                    help='How much extra room around tape bumps to exclude stars in units of FWHM')
 
 
 args = parser.parse_args()
+
+
+# Read and parse the tapebump file if we are going to need it.
+if args.use_tapebumps:
+    with open(args.tapebump_file) as fin:
+        raw_tbdata = [ line.split() for line in fin ]
+    # repackage this as  dict (key = ccdnum) of lists of tuples (xmin, ymin, xmax, ymax)
+    tbdata = {}
+    for d in raw_tbdata:
+        ccdnum = int(d[0])
+        if ccdnum not in tbdata:
+            tbdata[ccdnum] = []
+        tbdata[ccdnum].append( (int(d[1]), int(d[2]), int(d[3]), int(d[4])) )
+    print 'read in tape bump file.  %d bumps for %d chips'%(len(raw_tbdata),len(tbdata))
+
+# This will be the function to process the tapebump data for a particular chip:
+def exclude_tapebumps(tbd, data, extra):
+    """
+    tbd = tbdata for a particular ccdnum,
+    data = the input data for the stars
+    extra = how much extra distance around the tape bumps to exclude stars in pixels
+    """
+    x = data['X_IMAGE']
+    y = data['Y_IMAGE']
+    masks = [(x>tb[0]-extra) & (x<tb[2]+extra) & (y>tb[1]-extra) & (y<tb[3]+extra) for tb in tbd]
+    mask = np.any(masks, axis=0)
+    print '   masking %d stars for being in or near a bump'%sum(mask)
+    return data[mask]
 
 
 for run,exp in zip(args.runs,args.exps):
@@ -126,15 +161,16 @@ for run,exp in zip(args.runs,args.exps):
             img_file=odir+'/'+base_file
         hdu=0
 
-        # This is the file that holds the vignettes 
-        psfcat_file=odir+'/'+root+'_psfcat.fits'
+        # Get the chipnum.  We could do this by reading the file and looking for the 
+        # CCDNUM keyword.  But it's simpler to just use the filename.  We should be
+        # able to assume that the file names are not mangled.
+        ccdnum = int(root.split('_')[-1])
+        print '   ccdnum = ',ccdnum
 
-        # if the sextractor catalog does not exist 
-        if not os.path.exists(psfcat_file):
-
-            # extract the saturation level, this is how desdm runs sextractor
-            # we need the fwhm for class star
-            pyfile=pyfits.open(img_file)
+        # extract the saturation level, this is how desdm runs sextractor
+        # we need the fwhm for class star
+        # Also need the fwhm for doing the tape bumps.
+        with pyfits.open(img_file) as pyfile:
             sat=-1
             fwhm=4.
             try:
@@ -143,8 +179,18 @@ for run,exp in zip(args.runs,args.exps):
             except:
                 print 'Cannot process ',img_file
                 continue
-            
-            cat_cmd="{cat_dir}/sex {img_file}[0] -c {cat_config} -CATALOG_NAME {cat_file} -CATALOG_TYPE FITS_LDAC -WEIGHT_TYPE MAP_WEIGHT -WEIGHT_IMAGE {img_file}[2] -PARAMETERS_NAME {param_file} -FILTER_NAME {filter_file}  -STARNNW_NAME {star_file} -DETECT_MINAREA 3 -SEEING_FWHM {fwhm} -SATUR_LEVEL {sat}  >>{logfile} 2>&1".format(cat_dir=args.cat_dir, img_file=img_file, cat_config=args.config_cat,cat_file=psfcat_file,param_file=args.param_file,filter_file=args.filt_file,star_file=args.star_file,fwhm=fwhm,logfile=logfile,sat=sat)
+            print '   fwhm = ',fwhm
+
+        # This is the file that holds the vignettes 
+        psfcat_file=odir+'/'+root+'_psfcat.fits'
+
+        # if the sextractor catalog does not exist 
+        if not os.path.exists(psfcat_file):
+            print '   running sextractor'
+            cat_cmd="{cat_dir}/sex {img_file}[0] -c {cat_config} -CATALOG_NAME {cat_file} -CATALOG_TYPE FITS_LDAC -WEIGHT_TYPE MAP_WEIGHT -WEIGHT_IMAGE {img_file}[2] -PARAMETERS_NAME {param_file} -FILTER_NAME {filter_file}  -STARNNW_NAME {star_file} -DETECT_MINAREA 3 -SEEING_FWHM {fwhm} -SATUR_LEVEL {sat}  >>{logfile} 2>&1".format(
+                cat_dir=args.cat_dir, img_file=img_file, cat_config=args.config_cat,
+                cat_file=psfcat_file,param_file=args.param_file,filter_file=args.filt_file,
+                star_file=args.star_file,fwhm=fwhm,logfile=logfile,sat=sat)
             os.system(cat_cmd)
 
         # if we want to use only the stars selected by findstars
@@ -153,7 +199,9 @@ for run,exp in zip(args.runs,args.exps):
             star_file=odir+'/'+root+'_findstars.fits'    
 
             # run find stars
-            findstars_cmd='%s/findstars %s root=%s cat_ext=_psfcat.fits stars_file=%s input_prefix=%s/ >>%s 2>&1'%(args.findstars_dir,args.config_findstars,root,star_file,odir,logfile)
+            print '   running findstars'
+            findstars_cmd='%s/findstars %s root=%s cat_ext=_psfcat.fits stars_file=%s input_prefix=%s/ >>%s 2>&1'%(
+                    args.findstars_dir,args.config_findstars,root,star_file,odir,logfile)
             os.system(findstars_cmd)
             
             wlcat_file=psfcat_file.replace('psfcat','psfcat_findstars')
@@ -181,21 +229,35 @@ for run,exp in zip(args.runs,args.exps):
                 psfcat_file=wlcat_file
 
         # If we want to cut the brighest magnitudes
-        if args.mag_cut>0:
-            magcut_file=psfcat_file.replace('psfcat','psfcat_magcut_%0.1f'%args.mag_cut)
+        if args.mag_cut>0 or args.use_tapebumps:
 
-            # get the brightest 10 mags that have flags=0 and take the median just in case some were
-            # selected
+            # get the brightest 10 stars that have flags=0 and take the median just in case some
+            # strange magnitudes were selected
             hdu=2
             pyfile=pyfits.open(psfcat_file)
             flags_mask=pyfile[hdu].data['FLAGS']==0
-            mags=pyfile[hdu].data['MAG_AUTO'][flags_mask]
-            mags.sort()
-            min_star=np.median(mags[0:args.nstars])
+            data=pyfile[hdu].data
+            print '   len(data) = ',len(data)
+
+            if args.mag_cut > 0:
+                magcut_file=psfcat_file.replace('psfcat','psfcat_magcut_%0.1f'%args.mag_cut)
+                mags=data['MAG_AUTO'][flags_mask]
+                mags.sort()
+                min_star=np.median(mags[0:args.nstars])
+                print '   min mag = ',mags[0]
+                print '   median of brightest %d is '%args.nstars,min_star
             
-            mag_mask=pyfile[hdu].data['MAG_AUTO']>min_star+args.mag_cut
+                mag_mask=data['MAG_AUTO']>min_star+args.mag_cut
+                print '   select stars dimmer than ',min_star+args.mag_cut
+                print '   which includes %d stars'%np.count_nonzero(mag_mask)
             
-            data=pyfile[hdu].data[mag_mask]
+                data=data[mag_mask]
+                print '   after exclude bright: len(data) = ',len(data)
+
+            if args.use_tapebumps:
+                print '   tapebumps = ',tbdata[ccdnum]
+                data = exclude_tapebumps(tbdata[ccdnum], data, args.tapebump_extra * fwhm)
+                print '   after exclude tapebumps: len(data) = ',len(data)
         
             # Need to make different copy of these to not fail
             hdu1=copy.copy(pyfile[0])
@@ -217,12 +279,13 @@ for run,exp in zip(args.runs,args.exps):
         catout_file=psfcat_file.replace('fits','used.fits')
     
         if args.run_psfex:
-            psf_cmd=('%s/psfex %s -c %s -OUTCAT_TYPE FITS_LDAC -OUTCAT_NAME %s >>%s 2>&1' % (args.psf_dir,psfcat_file,args.config_psf,catout_file,logfile))
+            print '   running psfex'
+            psf_cmd=('%s/psfex %s -c %s -OUTCAT_TYPE FITS_LDAC -OUTCAT_NAME %s >>%s 2>&1' % (
+                        args.psf_dir,psfcat_file,args.config_psf,catout_file,logfile))
             os.system(psf_cmd)
         
-        if did_unpack and args.rm_files:
-
+        if do_unpack and args.rm_files:
             rm_cmd='rm %s >>%s 2>&1'%(img_file,logfile)
             os.system(rm_cmd)
         
-        break
+

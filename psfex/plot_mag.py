8f79253b62fd95d7e2a179af60e06c5408dcebd2
Mike Jarvis <michael@jarvis.net>
2015-01-11 22:50:33 -0500

    Add plots split by airmass, fwhm, sky.  Also some improvements to existing plots.

--- psfex/plot_mag.py
+++ psfex/plot_mag.py
@@ -52,10 +52,20 @@ def break_axes(ax, ax2):
     ax2.plot((-d,+d),(-d,+d), **kwargs)
     ax2.plot((-d,+d),(1-d,1+d), **kwargs)
 
+def add_to_list(filter, vlist, value):
+    vlist['griz'].append(value)
+    if filter not in vlist.keys():
+        vlist[filter] = []
+    if 'g' not in filter:
+        vlist['riz'].append(value)
+    vlist[filter].append(value)
+
 def get_data(runs, exps, work,
-             mask_list, used_list, ccd_list,
+             mask_list, used_list, ccd_list, 
+             airmass_list, sky_list, sigsky_list, fwhm_list,
              ra_list, dec_list, x_list, y_list, m_list,
-             e1_list, e2_list, s_list, pe1_list, pe2_list, ps_list):
+             e1_list, e2_list, s_list, pe1_list, pe2_list, ps_list,
+             psfex = 'psfex'):
 
     import astropy.io.fits as pyfits
     import numpy
@@ -69,102 +79,67 @@ def get_data(runs, exps, work,
 
         print 'Start work on run, exp = ',run,exp
         expnum = int(exp[6:])
-        print 'expnum = ',expnum
+        #print 'expnum = ',expnum
 
         if expnum not in expinfo['expnum']:
             print 'expnum is not in expinfo!'
             print 'expinfo[expnum] = ',expinfo['expnum']
             raise RuntimeError("Could not find information about this expnum")
         k = numpy.nonzero(expinfo['expnum'] == expnum)[0][0]
-        print 'k = ',k
+        #print 'k = ',k
         filter = expinfo['filter'][k]
-        print 'filter[k] = ',filter
+        print 'filter = ',filter
 
         exp_dir = os.path.join(work,exp)
-        print 'exp_dir = ',exp_dir
+        #print 'exp_dir = ',exp_dir
 
         cat_file = os.path.join(exp_dir, exp + "_psf.fits")
         with pyfits.open(cat_file) as pyf:
             data = pyf[1].data.copy()
 
-        mask = (data['flag'] <= 1)
+        #print 'max flag = ',max(data['flag'])
+        #print 'min flag = ',min(data['flag'])
+        if min(data['flag']) < 0:
+            print '!!!Flag is negative!!!'
+        mask = (data['flag'] == 0) | (data['flag'] == 1)
         if mask.sum() == 0:
             print 'All objects in this exposure are flagged.'
             print 'Probably due to astrometry flags. Skip this exposure.'
             continue
 
         used = (data['flag'] == 0)
+        bright = mask & (data['mag'] < 10.2)
 
-        mask_list['griz'].append(mask)
-        used_list['griz'].append(used)
-        ccd_list['griz'].append(data['ccdnum'])
+        #print 'nobject = ',sum(mask)
+        #print 'nused = ',sum(used)
+        #print 'full mag range = ',min(data['mag']),max(data['mag'])
+        #print 'masked mag range = ',min(data['mag'][mask]),max(data['mag'][mask])
+        #print 'used mag range = ',min(data['mag'][used]),max(data['mag'][used])
+        #print 'alt used mag range = ',min(data[used]['mag']),max(data[used]['mag'])
 
-        ra_list['griz'].append(data['ra'])
-        dec_list['griz'].append(data['dec'])
-        x_list['griz'].append(data['x'])
-        y_list['griz'].append(data['y'])
-        m_list['griz'].append(data['mag'])
+        add_to_list(filter, mask_list, mask)
+        add_to_list(filter, used_list, used)
+        add_to_list(filter, ccd_list, data['ccdnum'])
 
-        e1_list['griz'].append(data['e1'])
-        e2_list['griz'].append(data['e2'])
-        s_list['griz'].append(data['size'])
+        add_to_list(filter, ra_list, data['ra'])
+        add_to_list(filter, dec_list, data['dec'])
+        add_to_list(filter, x_list, data['x'])
+        add_to_list(filter, y_list, data['y'])
+        add_to_list(filter, m_list, data['mag'])
 
-        pe1_list['griz'].append(data['psfex_e1'])
-        pe2_list['griz'].append(data['psfex_e2'])
-        ps_list['griz'].append(data['psfex_size'])
+        add_to_list(filter, e1_list, data['e1'])
+        add_to_list(filter, e2_list, data['e2'])
+        add_to_list(filter, s_list, data['size'])
 
-        if filter not in ra_list.keys():
-            mask_list[filter] = []
-            used_list[filter] = []
-            ccd_list[filter] = []
-            ra_list[filter] = []
-            dec_list[filter] = []
-            x_list[filter] = []
-            y_list[filter] = []
-            m_list[filter] = []
-            e1_list[filter] = []
-            e2_list[filter] = []
-            s_list[filter] = []
-            pe1_list[filter] = []
-            pe2_list[filter] = []
-            ps_list[filter] = []
+        add_to_list(filter, pe1_list, data[psfex + '_e1'])
+        add_to_list(filter, pe2_list, data[psfex + '_e2'])
+        add_to_list(filter, ps_list, data[psfex + '_size'])
 
-        if 'g' not in filter:
-            mask_list['riz'].append(mask)
-            used_list['riz'].append(used)
-            ccd_list['riz'].append(data['ccdnum'])
-
-            ra_list['riz'].append(data['ra'])
-            dec_list['riz'].append(data['dec'])
-            x_list['riz'].append(data['x'])
-            y_list['riz'].append(data['y'])
-            m_list['riz'].append(data['mag'])
-
-            e1_list['riz'].append(data['e1'])
-            e2_list['riz'].append(data['e2'])
-            s_list['riz'].append(data['size'])
-
-            pe1_list['riz'].append(data['psfex_e1'])
-            pe2_list['riz'].append(data['psfex_e2'])
-            ps_list['riz'].append(data['psfex_size'])
-
-        mask_list[filter].append(mask)
-        used_list[filter].append(used)
-        ccd_list[filter].append(data['ccdnum'])
-
-        ra_list[filter].append(data['ra'])
-        dec_list[filter].append(data['dec'])
-        x_list[filter].append(data['x'])
-        y_list[filter].append(data['y'])
-        m_list[filter].append(data['mag'])
-
-        e1_list[filter].append(data['e1'])
-        e2_list[filter].append(data['e2'])
-        s_list[filter].append(data['size'])
-
-        pe1_list[filter].append(data['psfex_e1'])
-        pe2_list[filter].append(data['psfex_e2'])
-        ps_list[filter].append(data['psfex_size'])
+        n = len(mask)
+        add_to_list(filter, airmass_list, [expinfo['airmass'][k]] * n)
+        add_to_list(filter, sky_list, [expinfo['sky'][k]] * n)
+        add_to_list(filter, sigsky_list, [expinfo['sigsky'][k]] * n)
+        add_to_list(filter, fwhm_list, [expinfo['fwhm'][k]] * n)
 
     print '\nFinished processing all exposures'
 
@@ -213,20 +188,29 @@ def bin_by_mag(m, ds, de1, de2, key):
     print 'bin_ds = ',bin_ds
 
     plt.clf()
-    plt.title('PSF Size residuals')
+    #plt.title('PSF Size residuals')
     plt.xlim(10,16.5)
-    plt.ylim(-0.002,0.012)
+    if 'orig' in key:
+        plt.ylim(0.3,0.7)
+        plt.ylabel(r'$size_{\rm psf}$ (arcsec)')
+    elif 'model' in key:
+        plt.ylim(0.3,0.7)
+        plt.ylabel(r'$size_{\rm model}$ (arcsec)')
+    else:
+        plt.ylim(-0.002,0.012)
+        plt.ylabel(r'$size_{\rm psf} - size_{\rm model}$ (arcsec)')
     plt.plot([10,16.5], [0,0], color='black')
     plt.plot([13,13],[-1,1], color='red')
     plt.fill( [10,10,13,13], [-1,1,1,-1], fill=False, hatch='/', color='red')
     plt.errorbar(mag_bins[:-1], bin_ds, yerr=bin_ds_err, color='blue', fmt='o')
     plt.xlabel('Uncorrected Magnitude')
-    plt.ylabel('$size_{psf} - size_{model}$')
+    #plt.ylabel(r'$size_{\rm psf} - \langle size_{\rm psf} \rangle$ (arcsec)')
     plt.savefig('dsize_mag_' + key + '.png')
     plt.savefig('dsize_mag_' + key + '.pdf')
+    plt.savefig('dsize_mag_' + key + '.eps')
 
     plt.clf()
-    plt.title('PSF Ellipticity residuals')
+    #plt.title('PSF Ellipticity residuals')
     plt.xlim(10,16.5)
     plt.ylim(-3.e-4,8.e-4)
     plt.plot([10,16.5], [0,0], color='black')
@@ -236,9 +220,11 @@ def bin_by_mag(m, ds, de1, de2, key):
     e2_line = plt.errorbar(mag_bins[:-1], bin_de2, yerr=bin_de2_err, color='green', fmt='o')
     plt.legend([e1_line, e2_line], [r'$e_1$', r'$e_2$'])
     plt.xlabel('Uncorrected Magnitude')
-    plt.ylabel('$e_{psf} - e_{model}$')
+    #plt.ylabel(r'$e_{\rm psf} - \langle e_{\rm psf} \rangle$')
+    plt.ylabel(r'$e_{\rm psf} - e_{\rm model}$')
     plt.savefig('de_mag_' + key + '.png')
     plt.savefig('de_mag_' + key + '.pdf')
+    plt.savefig('de_mag_' + key + '.eps')
 
 
 def bin_by_chip_pos(x, ds, de1, de2, key, xy):
@@ -298,6 +284,7 @@ def bin_by_chip_pos(x, ds, de1, de2, key, xy):
     plt.ylabel('$size_{psf} - size_{model}$')
     plt.savefig('dsize_'+xy+'_' + key + '.png')
     plt.savefig('dsize_'+xy+'_' + key + '.pdf')
+    plt.savefig('dsize_'+xy+'_' + key + '.eps')
 
     plt.clf()
     plt.title('PSF Ellipticity residuals')
@@ -311,6 +298,7 @@ def bin_by_chip_pos(x, ds, de1, de2, key, xy):
     plt.ylabel('$e_{psf} - e_{model}$')
     plt.savefig('de_'+xy+'_' + key + '.png')
     plt.savefig('de_'+xy+'_' + key + '.pdf')
+    plt.savefig('de_'+xy+'_' + key + '.eps')
 
     # Make broken x-axis. 
     plt.clf()
@@ -334,6 +322,7 @@ def bin_by_chip_pos(x, ds, de1, de2, key, xy):
 
     plt.savefig('dsize_'+xy+'2_' + key + '.png')
     plt.savefig('dsize_'+xy+'2_' + key + '.pdf')
+    plt.savefig('dsize_'+xy+'2_' + key + '.eps')
 
     plt.clf()
     f, (ax,ax2) = plt.subplots(1,2,sharey=True)
@@ -359,6 +348,7 @@ def bin_by_chip_pos(x, ds, de1, de2, key, xy):
 
     plt.savefig('de_'+xy+'2_' + key + '.png')
     plt.savefig('de_'+xy+'2_' + key + '.pdf')
+    plt.savefig('de_'+xy+'2_' + key + '.eps')
 
 
 def bin_by_fov(ccd, x, y, ds, de1, de2, key):
@@ -373,12 +363,13 @@ def bin_by_fov(ccd, x, y, ds, de1, de2, key):
     all_e2 = numpy.array([])
     all_s = numpy.array([])
 
-    x_bins = numpy.linspace(0,2048,9)
-    y_bins = numpy.linspace(0,4096,17)
+    nwhisk = 5
+    x_bins = numpy.linspace(0,2048,nwhisk+1)
+    y_bins = numpy.linspace(0,4096,2*nwhisk+1)
 
     ccdnums = numpy.unique(ccd)
     for ccdnum in ccdnums:
-        mask = (ccd == ccdnum)
+        mask = numpy.where(ccd == ccdnum)[0]
         print 'ccdnum = ',ccdnum,', nstar = ',mask.sum()
         if mask.sum() < 100: continue
 
@@ -406,7 +397,7 @@ def bin_by_fov(ccd, x, y, ds, de1, de2, key):
 
         focal_x, focal_y = toFocal(ccdnum, bin_x, bin_y)
 
-        mask2 = (bin_count > 0)
+        mask2 = numpy.where(bin_count > 0)[0]
         print 'num with count > 0 = ',mask2.sum()
         all_x = numpy.append(all_x, focal_x[mask2])
         all_y = numpy.append(all_y, focal_y[mask2])
@@ -416,22 +407,30 @@ def bin_by_fov(ccd, x, y, ds, de1, de2, key):
 
 
     plt.clf()
-    plt.title('PSF Ellipticity residuals in DES focal plane')
+    #plt.title('PSF Ellipticity residuals in DES focal plane')
     theta = numpy.arctan2(all_e2,all_e1)/2.
     r = numpy.sqrt(all_e1**2 + all_e2**2)
     u = r*numpy.cos(theta)
     v = r*numpy.sin(theta)
     plt.xlim(-250,250)
     plt.ylim(-250,250)
+    print 'all_x = ',all_x
+    print 'len(all_x) = ',len(all_x)
     qv = plt.quiver(all_x,all_y,u,v, pivot='middle', scale_units='xy',
                     headwidth=0., headlength=0., headaxislength=0.,
-                    width=0.001, scale=6.e-4, color='blue')
-    plt.quiverkey(qv, 0.10, 0.08, 0.01, "de = " + str(0.01),
+                    width=0.001, scale=1.e-3, color='blue')
+    ref_scale = 0.01
+    if 'raw' in key:
+        ref_label = 'e = ' + str(ref_scale)
+    else:
+        ref_label = 'de = ' + str(ref_scale)
+    plt.quiverkey(qv, 0.10, 0.08, ref_scale, ref_label,
                   coordinates='axes', color='darkred', labelcolor='darkred',
                   labelpos='E', fontproperties={'size':'x-small'})
     plt.axis('off')
     plt.savefig('de_fov_' + key + '.png')
     plt.savefig('de_fov_' + key + '.pdf')
+    plt.savefig('de_fov_' + key + '.eps')
 
  
 
@@ -462,6 +461,11 @@ def main():
     used_list = { 'griz' : [], 'riz' : [] }
     ccd_list = { 'griz' : [], 'riz' : [] }
 
+    airmass_list = { 'griz' : [], 'riz' : [] }
+    sky_list = { 'griz' : [], 'riz' : [] }
+    sigsky_list = { 'griz' : [], 'riz' : [] }
+    fwhm_list = { 'griz' : [], 'riz' : [] }
+
     ra_list = { 'griz' : [], 'riz' : [] }
     dec_list = { 'griz' : [], 'riz' : [] }
     x_list = { 'griz' : [], 'riz' : [] }
@@ -478,20 +482,43 @@ def main():
 
     get_data(runs, exps, work,
              mask_list, used_list, ccd_list,
+             airmass_list, sky_list, sigsky_list, fwhm_list,
              ra_list, dec_list, x_list, y_list, m_list,
-             e1_list, e2_list, s_list, pe1_list, pe2_list, ps_list)
+             e1_list, e2_list, s_list, pe1_list, pe2_list, ps_list, psfex='psfex')
 
-    for key in ra_list.keys():
-    #for key in ['riz']:
+    #for key in ra_list.keys():
+    for key in ['riz']:
         mask = numpy.concatenate(mask_list[key])
         used = numpy.concatenate(used_list[key])
         ccd = numpy.concatenate(ccd_list[key])
+
+        airmass = numpy.concatenate(airmass_list[key])
+        sky = numpy.concatenate(sky_list[key])
+        sigsky = numpy.concatenate(sigsky_list[key])
+        fwhm = numpy.concatenate(fwhm_list[key])
+
+        med_airmass = numpy.median(airmass)
+        med_sky = numpy.median(sky)
+        med_sigsky = numpy.median(sigsky)
+        med_fwhm = numpy.median(fwhm)
+        print 'airmass: ',min(airmass),med_airmass,max(airmass)
+        print 'sky: ',min(sky),med_sky,max(sky)
+        print 'sigsky: ',min(sigsky),med_sigsky,max(sigsky)
+        print 'fwhm: ',min(fwhm),med_fwhm,max(fwhm)
 
         ra = numpy.concatenate(ra_list[key])
         dec = numpy.concatenate(dec_list[key])
         x = numpy.concatenate(x_list[key])
         y = numpy.concatenate(y_list[key])
         m = numpy.concatenate(m_list[key])
+        print 'full mag range = ',min(m),max(m)
+        print 'masked mag range = ',min(m[mask]),max(m[mask])
+        print 'used mag range = ',min(m[used]),max(m[used])
+        print 'len(m) = ',len(m)
+        print 'len(mask) = ',len(mask)
+        print 'len(used) = ',len(used)
+        print 'sum(mask) = ',sum(mask)
+        print 'sum(used) = ',sum(used)
 
         e1 = numpy.concatenate(e1_list[key])
         e2 = numpy.concatenate(e2_list[key])
@@ -508,16 +535,49 @@ def main():
         print 'mean s (used) = ',numpy.mean(s[used])
         print 'mean e1 (used) = ',numpy.mean(e1[used])
         print 'mean e2 (used) = ',numpy.mean(e2[used])
+        print 'mean ps (used) = ',numpy.mean(ps[used])
+        print 'mean pe1 (used) = ',numpy.mean(pe1[used])
+        print 'mean pe2 (used) = ',numpy.mean(pe2[used])
         de1 = e1 - pe1
         de2 = e2 - pe2
         ds = s - ps
+        print 'mean ds (used) = ',numpy.mean(ds[used])
+        print 'mean de1 (used) = ',numpy.mean(de1[used])
+        print 'mean de2 (used) = ',numpy.mean(de2[used])
 
         bin_by_mag(m[mask], ds[mask], de1[mask], de2[mask], key)
+        bin_by_mag(m[used], ds[used], de1[used], de2[used], key+'_used')
+        #bin_by_mag(m[mask], s[mask], e1[mask], e2[mask], key+'_orig')
+        #bin_by_mag(m[mask], ps[mask], pe1[mask], pe2[mask], key+'_model')
 
-        bin_by_chip_pos(x[used], ds[used], de1[used], de2[used], key, 'x')
-        bin_by_chip_pos(y[used], ds[used], de1[used], de2[used], key, 'y')
+        #mask2 = mask & (sky<med_sky)
+        #bin_by_mag(m[mask2], ds[mask2], de1[mask2], de2[mask2], key+'_lowsky')
+        #mask2 = mask & (sky>med_sky)
+        #bin_by_mag(m[mask2], ds[mask2], de1[mask2], de2[mask2], key+'_highsky')
+
+        #mask2 = mask & (sigsky<med_sigsky)
+        #bin_by_mag(m[mask2], ds[mask2], de1[mask2], de2[mask2], key+'_lowsigsky')
+        #mask2 = mask & (sigsky>med_sigsky)
+        #bin_by_mag(m[mask2], ds[mask2], de1[mask2], de2[mask2], key+'_highsigsky')
+
+        #mask2 = mask & (airmass<med_airmass)
+        #bin_by_mag(m[mask2], ds[mask2], de1[mask2], de2[mask2], key+'_lowairmass')
+        #mask2 = mask & (airmass>med_airmass)
+        #bin_by_mag(m[mask2], ds[mask2], de1[mask2], de2[mask2], key+'_highairmass')
+
+        #mask2 = mask & (fwhm<med_fwhm)
+        #bin_by_mag(m[mask2], ds[mask2], de1[mask2], de2[mask2], key+'_lowfwhm')
+        #mask2 = mask & (fwhm>med_fwhm)
+        #bin_by_mag(m[mask2], ds[mask2], de1[mask2], de2[mask2], key+'_highfwhm')
+
+        #bin_by_mag(m[mask], s[mask] - numpy.mean(s[mask]),
+        #           e1[mask] - numpy.mean(e1[mask]), e2[mask] - numpy.mean(e2[mask]), key)
+
+        #bin_by_chip_pos(x[used], ds[used], de1[used], de2[used], key, 'x')
+        #bin_by_chip_pos(y[used], ds[used], de1[used], de2[used], key, 'y')
 
         bin_by_fov(ccd[used], x[used], y[used], ds[used], de1[used], de2[used], key)
+        bin_by_fov(ccd[used], x[used], y[used], s[used], e1[used], e2[used], key + 'raw')
 
 
 if __name__ == "__main__":

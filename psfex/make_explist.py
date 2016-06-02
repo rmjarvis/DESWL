624a78656caa8c12036131c089b42eaf656e7308
Mike Jarvis <michael@jarvis.net>
2016-06-02 01:08:28 -0400

    make lists for y1all-002

--- psfex/make_explist.py
+++ psfex/make_explist.py
@@ -1,14 +1,11 @@
 import os, glob
+import fitsio
 
-dirs = [ 
-        '/astro/u/mjarvis/DES/meds/spt-e-gold',
-        '/astro/u/mjarvis/DES/meds/y1a1-alpha',
-        '/astro/u/mjarvis/DES/meds/y1a1-beta',
-        '/astro/u/mjarvis/DES/meds/y1a1-gamma',
-        '/astro/u/mjarvis/DES/meds/y1a1-delta',
-       ]
+meds_dir = '/astro/u/mjarvis/DES/meds'
+meds_tag = 'y1a1-spt-002'
+dirs = [ os.path.join(meds_dir, meds_tag) ]
 
-outfile = 'des2305-0124_exp'
+outfile = 'y1all-002'
 bands = ['g','r','i','z']
 #dir = '/astro/u/mjarvis/DES/meds/tb-y1a1-v01'
 #outfile = 'tb-y1a1'
@@ -18,18 +15,17 @@ for b in bands:
     print out_b
     exps = set()
     for dir in dirs:
-        pat = os.path.join(dir,'*','*-'+b+'-meds-srclist*.dat')
+        pat = os.path.join(dir,'*','*-'+b+'-meds-stubby-%s.fits'%meds_tag)
         print pat
         for srclist in glob.glob(pat):
-            if 'DES2305-0124' not in srclist:
-                continue
+            #if 'DES2305-0124' not in srclist:
+                #continue
             print srclist
-            with open(srclist,'r') as f:
-                for line in f:
-                    vals = line.split()
-                    #print vals[2]
-                    file = vals[2]
-                    # file name looks like /astro/u/astrodat/data/DES/OPS/red/$run/red/$exp/filename
+            with fitsio.FITS(srclist) as f:
+                files = f['image_info']['image_path'][:]
+                for file in files:
+                    if 'coadd' in file: continue
+                    # file name looks like ${DESDATA}/OPS/red/$run/red/$exp/filename
                     tokens = file.split('/')
                     run = tokens[-4]
                     exp = tokens[-2]

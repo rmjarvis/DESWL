357ed81c32cb29c046f12584a2626d9ea0557197
Mike Jarvis <michael@jarvis.net>
2022-08-27 20:30:42 -0700

    Do full phi range

--- 3pt/3pt.py
+++ 3pt/3pt.py
@@ -7,7 +7,6 @@ zbin1_files = sorted(glob.glob(os.path.join(data_dir, 'T17', 'patches_zbin1', '*
 zbin2_files = sorted(glob.glob(os.path.join(data_dir, 'T17', 'patches_zbin2', '*.fits')))
 zbin3_files = sorted(glob.glob(os.path.join(data_dir, 'T17', 'patches_zbin3', '*.fits')))
 zbin4_files = sorted(glob.glob(os.path.join(data_dir, 'T17', 'patches_zbin4', '*.fits')))
-mice_files = sorted(glob.glob(os.path.join(data_dir, 'MICE', '*.fits')))
 
 params_cat = dict(ra_col='ra', dec_col='dec', ra_units='rad', dec_units='rad',
                   w_col='w', g1_col='g1', g2_col='g2', flip_g1=True)
@@ -16,57 +15,19 @@ cats_1 = [treecorr.Catalog(f, params_cat, patch=p) for p,f in enumerate(zbin1_fi
 cats_2 = [treecorr.Catalog(f, params_cat, patch=p) for p,f in enumerate(zbin2_files)]
 cats_3 = [treecorr.Catalog(f, params_cat, patch=p) for p,f in enumerate(zbin3_files)]
 cats_4 = [treecorr.Catalog(f, params_cat, patch=p) for p,f in enumerate(zbin4_files)]
-cats_m = [treecorr.Catalog(f, params_cat, patch=p, w_col='0') for p,f in enumerate(mice_files)]
 
-if 0:
-    params_2pt = dict(min_sep=1, max_sep=200, sep_units='arcmin', nbins=10)
+wide = dict(min_sep=1, max_sep=5, sep_units='arcmin', nbins=5,
+            min_u=0.9, max_u=1, nubins=1,
+            min_v=0.0, max_v=1.0, nvbins=50, verbose=2)
 
-    gg = treecorr.GGCorrelation(params_2pt, var_method='bootstrap')
+narrow = dict(min_sep=1, max_sep=5, sep_units='arcmin', nbins=5,
+              min_u=0.0, max_u=1, nubins=20,
+              min_v=0.0, max_v=0.1, nvbins=1, verbose=2)
 
-    print('zbin1: GG')
-    gg.process(cats_2)
-    print(gg.xip)
-    print(gg.xim)
-    print(gg.varxip**0.5)
-    print(gg.varxim**0.5)
+ggg1 = treecorr.GGGCorrelation(wide, var_method='bootstrap')
+ggg1.process(cats_4)
+ggg1.write('wide.hdf', write_patch_results=True)
 
-    print('zbin4: GG')
-    gg.process(cats_4)
-    print(gg.xip)
-    print(gg.xim)
-    print(gg.varxip**0.5)
-    print(gg.varxim**0.5)
-
-    print('zbin1-4: NG')
-    ng = treecorr.NGCorrelation(params_2pt, var_method='bootstrap')
-    ng.process(cats_1, cats_4)
-    print(ng.xi)
-    print(ng.varxi**0.5)
-
-equilateral = dict(min_sep=1, max_sep=5, sep_units='arcmin', nbins=1,
-                   min_u=0.9, max_u=1, nubins=1,
-                   min_v=0, max_v=0.1, nvbins=1, verbose=2)
-isoceles = dict(min_sep=1, max_sep=5, sep_units='arcmin', nbins=5,
-                min_u=0.9, max_u=1, nubins=1,
-                min_v=0.7, max_v=1.0, nvbins=10, verbose=2)
-params_3pt = isoceles
-#params_3pt = equilateral
-ggg = treecorr.GGGCorrelation(params_3pt, var_method='bootstrap')
-ggg.process(cats_4)
-print(f'ntri = {ggg.ntri}')
-print(f'w = {ggg.weight}')
-print(f'meand1 = {ggg.meand1}')
-print(f'meand2 = {ggg.meand2}')
-print(f'meand3 = {ggg.meand3}')
-print(f'meanu = {ggg.meanu}')
-print(f'meanv = {ggg.meanv}')
-print(f'gam0 = {ggg.gam0}')
-print(f'gam1 = {ggg.gam1}')
-print(f'gam2 = {ggg.gam2}')
-print(f'gam3 = {ggg.gam3}')
-print(f'sigma_gam0 = {ggg.vargam0**0.5}')
-print(f'sigma_gam1 = {ggg.vargam1**0.5}')
-print(f'sigma_gam2 = {ggg.vargam2**0.5}')
-print(f'sigma_gam3 = {ggg.vargam3**0.5}')
-
-ggg.write('ggg.hdf', write_patch_results=True)
+ggg2 = treecorr.GGGCorrelation(narrow, var_method='bootstrap')
+ggg2.process(cats_4)
+ggg2.write('narrow.hdf', write_patch_results=True)
--- 3pt/plot_3pt.py
+++ 3pt/plot_3pt.py
@@ -2,53 +2,100 @@ import treecorr
 import numpy as np
 import matplotlib.pyplot as plt
 
-params_3pt = dict(min_sep=1, max_sep=5, sep_units='arcmin', nbins=5,
-                min_u=0.9, max_u=1, nubins=1,
-                min_v=0.7, max_v=1.0, nvbins=10, verbose=2)
-ggg = treecorr.GGGCorrelation(params_3pt, var_method='bootstrap')
-ggg.read('ggg.hdf')
+wide = dict(min_sep=1, max_sep=5, sep_units='arcmin', nbins=5,
+            min_u=0.9, max_u=1, nubins=1,
+            min_v=0.0, max_v=1.0, nvbins=50, verbose=2)
 
-u = ggg.meanu
-v = ggg.meanv
-d1 = ggg.meand1
-d2 = ggg.meand2
-d3 = ggg.meand3
+narrow = dict(min_sep=1, max_sep=5, sep_units='arcmin', nbins=5,
+              min_u=0.0, max_u=1, nubins=20,
+              min_v=0.0, max_v=0.1, nvbins=1, verbose=2)
 
-g_ttt = -0.25 * (ggg.gam0 + ggg.gam1 + ggg.gam2 + ggg.gam3).real
-var_ttt = 0.25**2 * (ggg.vargam0 + ggg.vargam1 + ggg.vargam2 + ggg.vargam3)
 
-phi = np.arccos( (d2**2 + d3**2 - d1**2) / (2*d2*d3) )
-phi *= 180/np.pi
+ggg_w = treecorr.GGGCorrelation(wide)
+ggg_w.read('wide.hdf')
+ggg_n = treecorr.GGGCorrelation(narrow)
+ggg_n.read('narrow.hdf')
 
-nr, nu, nv = phi.shape
+all_g_ttt = []
+all_sig_ttt = []
+all_meanr = []
+all_phi = []
 
-print(nr,nu,nv)
-assert nu == 1
-assert nv % 2 == 0
-nv //= 2
+for ggg in [ggg_n, ggg_w]:
+
+    g_ttt = -0.25 * (ggg.gam0 + ggg.gam1 + ggg.gam2 + ggg.gam3).real
+    var_ttt = 0.25**2 * (ggg.vargam0 + ggg.vargam1 + ggg.vargam2 + ggg.vargam3)
+
+    nr, nu, nv = g_ttt.shape
+    print(nr,nu,nv)
+    assert nv % 2 == 0
+    nv //= 2
+
+    assert nu == 1 or nv == 1
+    d1 = ggg.meand1
+    d2 = ggg.meand2
+    d3 = ggg.meand3
+    if nu == 1:
+        # if nu==1, then u=1, so d2 = d3, and phi is between d2 and d3
+        phi = np.arccos( (d2**2 + d3**2 - d1**2) / (2*d2*d3) )
+        meanr = np.array([np.mean([d2[ir], d3[ir]]) for ir in range(nr)])
+    else:
+        # if nv==1, then v=0, so d1 = d2, and phi is between d1 and d2
+        phi = np.arccos( (d1**2 + d2**2 - d3**2) / (2*d1*d2) )
+        meanr = np.array([np.mean([d1[ir], d2[ir]]) for ir in range(nr)])
+    phi *= 180/np.pi
+
+    # We don't care about v>0 vs v<0, so combine them.
+    phi = (phi[:,:,nv-1::-1] + phi[:,:,nv:]) / 2
+    g_ttt = (g_ttt[:,:,nv-1::-1] + g_ttt[:,:,nv:]) / 2
+    var_ttt = (var_ttt[:,:,nv-1::-1] + var_ttt[:,:,nv:]) / 4
+    sig_ttt = var_ttt**0.5
+
+    print('shapes:')
+    print('phi: ',phi.shape)
+    print('g_ttt: ',g_ttt.shape)
+    print('sig_ttt: ',sig_ttt.shape)
+    print('meanr: ',meanr.shape)
+
+    print('meanr =  ',meanr)
+
+    if nu == 1:
+        phi = phi[:,0,:]
+        g_ttt = g_ttt[:,0,:]
+        sig_ttt = sig_ttt[:,0,:]
+    else:
+        phi = phi[:,:,0]
+        g_ttt = g_ttt[:,:,0]
+        sig_ttt = sig_ttt[:,:,0]
+
+    print('shapes ->')
+    print('phi: ',phi.shape)
+    print('g_ttt: ',g_ttt.shape)
+    print('sig_ttt: ',sig_ttt.shape)
+
+    all_phi.append(phi)
+    all_g_ttt.append(g_ttt)
+    all_sig_ttt.append(sig_ttt)
+    all_meanr.append(meanr)
+
+phi = np.concatenate(all_phi, axis=1)
+g_ttt = np.concatenate(all_g_ttt, axis=1)
+sig_ttt = np.concatenate(all_sig_ttt, axis=1)
+meanr = np.concatenate(all_meanr, axis=0)
 
 fig, ax = plt.subplots()
 
 lines = []
 for ir in range(nr):
-    meanr = np.mean([d2[ir], d3[ir]])
-    _phi = phi[ir][0]
-
-    # We don't care about v>0 vs v<0, so combine them.
-    _phi = (phi[ir][0][nv-1::-1] + phi[ir][0][nv:]) / 2
-    _g = (g_ttt[ir][0][nv-1::-1] + g_ttt[ir][0][nv:]) / 2
-    _var = (var_ttt[ir][0][nv-1::-1] + var_ttt[ir][0][nv:]) / 4
-    _sig = _var**0.5
-
     print('ir = ',ir)
-    print('meanr = ',meanr)
-    print('phi = ',_phi)
-    print('g = ',_g)
-    print('sig = ',_sig)
+    print('meanr = ',meanr[ir])
+    print('phi = ',phi[ir])
+    print('g = ',g_ttt[ir])
+    print('sig = ',sig_ttt[ir])
     print()
 
-    line = ax.errorbar(_phi, _g, _sig)
-    lines.append((line, 'd2 ~= %.1f'%meanr))
+    line = ax.errorbar(phi[ir], g_ttt[ir], sig_ttt[ir])
+    lines.append((line, 'd2 ~= %.1f'%meanr[ir]))
 
 ax.legend(*(list(zip(*lines))))
 ax.set_xlabel(r'Opening angle $\phi$ [deg]')
